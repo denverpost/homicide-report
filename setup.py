05d00c19a4b0c11bfb68f204534d317b3b320ac1
freejoe76 <joe.murphy@gmail.com>
2015-01-14 18:07:48 -0700

    Give this setup.py a try

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+from setuptools import setup, Command
+import subprocess
+
+
+class PyTest(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        errno = subprocess.call(['py.test'])
+        raise SystemExit(errno)
+
+setup(
+    name='HomicideReport',
+    version='0.1',
+    description='A publishing tool for Google spreadsheets',
+    license='Apache',
+    url='https://github.com/denverpost/homicide-report',
+    author='Joe Murphy',
+    author_email='joe.murphy@gmail.com',
+    packages=['homicide-report'],
+    tests_require=['pytest'],
+    cmdclass={'test': PyTest},
+    )
+
