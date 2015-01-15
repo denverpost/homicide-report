dcca06b3023ae0a45045fbdba6df3d9dbbc97cf4
freejoe76 <joe.murphy@gmail.com>
2015-01-14 17:43:56 -0700

    Flesh out the class

--- spreadsheet.py
+++ spreadsheet.py
@@ -10,39 +10,62 @@ from optparse import OptionParser
 
 class Sheet:
     """ Handle google spreadsheet read and flatfile write operations.
-        >>> sheet = Sheet()
+        >>> sheet = Sheet('test-sheet', 'worksheet-name')
         >>> sheet.publish()
         """
 
-    def __init__(self):
+    def __init__(self, sheet_name, worksheet=None):
         # self.fw = FileWrapper()
         self.verbose = True
         self.directory = os.path.dirname(os.path.realpath(__file__))
         self.spread = gspread.login(os.environ.get('ACCOUNT_USER'), os.environ.get('ACCOUNT_KEY'))
+        self.sheet_name = sheet_name
+        if worksheet:
+            self.open_sheet(worksheet)
+            self.worksheet = worksheet
 
     def slugify(self, slug):
         return slug.lower().replace(' ', '-')
 
-    def publish(self, worksheet, options):
-        """ Publish the homicide data in whatever permutations we need.
-            >>> sheet = Sheet()
+    def open_sheet(self, worksheet, options=None):
+        """ Open a spreadsheet, return a sheet object.
+            >>> sheet = Sheet('test-sheet')
+            >>> sheet.open_sheet('worksheet-name')
             """
-        sheet = self.spread.open('homicides').worksheet('homicides')
-        rows = sheet.get_all_values()
+        self.sheet = self.spread.open(self.sheet_name).worksheet(worksheet)
+        return self.sheet
+
+    def publish(self, worksheet=None):
+        """ Publish the homicide data in whatever permutations we need.
+            >>> sheet = Sheet('test-sheet', 'worksheet-name')
+            >>> sheet.publish()
+            True
+            """
+        if not self.sheet or worksheet:
+            self.sheet = self.open_sheet(worksheet)
+
+        if not worksheet:
+            worksheet = self.worksheet
+
+        rows = self.sheet.get_all_values()
         keys = rows[0]
         fn = {
-            json: open('%s/output/%s.json' % (self.directory, worksheet), 'w'),
-            csv: open('%s/output/%s.csv' % (self.directory, worksheet), 'w')
+            'json': open('%s/output/%s.json' % (self.directory, worksheet), 'w'),
+            'csv': open('%s/output/%s.csv' % (self.directory, worksheet), 'w')
         }
         for i, row in enumerate(rows):
             if i == 0:
                 keys = row
                 continue
+            record = dict(zip(keys, row))
+            print record
+
+        return True
 
 
 def main():
     sheet = Sheet()
-    sheet.publish()
+    sheet.publish('Homicide Report', 'responses')
 
 if __name__ == '__main__':
     main()
--- tests/test_spreadsheet.py
+++ tests/test_spreadsheet.py
@@ -1,1 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import pytest
+from spreadsheet import Sheet
 
+def test_publish():
+    """ Test the Sheet's publish method.
+        """
+    sheet = Sheet('test-sheet', 'worksheet-name')
+    sheet.publish()
