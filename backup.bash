b3458c9b6da8935b797e05923df22922e99e0cd7
freejoe76 <jmurphy@denverpost.com>
2015-02-11 11:47:09 -0700

    Get the backup script working

--- backup.bash
+++ backup.bash
@@ -10,3 +10,11 @@ CURRENT_YEAR=2015
 cp output/responses.csv output/current.csv
 python homicide.py
 DIFF=`diff output/responses.csv output/current.csv`
+
+# If there was a difference, save the new spreadsheet in the backup dir.
+if [ ! -z "$DIFF" ]; then 
+    mkdir backup
+    cp output/responses.csv backup/`date +'%F-%H'`.csv
+fi
+
+rm output/current.csv
