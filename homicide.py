3f697d23c3f88cf71af7d9e58e4722b75d3c1744
freejoe76 <jmurphy@denverpost.com>
2015-04-06 11:52:16 -0400

    Fix unicode error. Fix https://github.com/denverpost/homicide-report/issues/35

--- homicide.py
+++ homicide.py
@@ -5,6 +5,7 @@ import sys
 import json
 import doctest
 import csv
+import codecs, cStringIO
 import gspread
 from spreadsheet import Sheet
 from collections import defaultdict
@@ -14,6 +15,36 @@ except ImportError:
     # python 2.6 or earlier, use backport
     from ordereddict import OrderedDict
 from optparse import OptionParser
+
+
+class UnicodeWriter:
+    """
+    A CSV writer which will write rows to CSV file "f",
+    which is encoded in the given encoding.
+    """
+
+    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
+        # Redirect output to a queue
+        self.queue = cStringIO.StringIO()
+        self.writer = csv.writer(self.queue, dialect=dialect, **kwds)
+        self.stream = f
+        self.encoder = codecs.getincrementalencoder(encoding)()
+
+    def writerow(self, row):
+        self.writer.writerow([s.encode("utf-8") for s in row])
+        # Fetch UTF-8 output from the queue ...
+        data = self.queue.getvalue()
+        data = data.decode("utf-8")
+        # ... and reencode it into the target encoding
+        data = self.encoder.encode(data)
+        # write to the target stream
+        self.stream.write(data)
+        # empty queue
+        self.queue.truncate(0)
+
+    def writerows(self, rows):
+        for row in rows:
+            self.writerow(row)
 
 
 class Homicide:
@@ -53,8 +84,8 @@ class Homicide:
             'jsonp': open('%s/output/%s.jsonp' % (self.sheet.directory, self.sheet.filename), 'wb'),
             'csv': open('%s/output/%s.csv' % (self.sheet.directory, self.sheet.filename), 'wb')
         }
-        recordwriter = csv.writer(
-            fn['csv'], delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        recordwriter = UnicodeWriter(fn['csv'], delimiter=',', 
+                                     quotechar='"', quoting=csv.QUOTE_MINIMAL)
         records = []
         for i, row in enumerate(rows):
             if i == 0:
